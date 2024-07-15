2b18812c78282ac9a1366ad119764e00f6da0010
ajosef <ajosef@opentext.com>
2024-07-15 23:58:04 +0530

    some refactoring

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 //parsing the json from the file into json object
 const tasksJson = JSON.parse(fs.readFileSync('./task.json'));
+const tasksArray = tasksJson.tasks;
+const tasksJsonString = JSON.stringify(tasksArray);
+// console.log(tasksArray);
 const app = express();
 const port = 3000;
 
@@ -18,31 +21,30 @@ const generateId = () => {
 
 
 // Input validation helper
-const validateTask = (task) => {
-    if (!task.title || !task.description) {
-      throw new Error('Title and description are required');
+ const validateTask = (task) => {
+    if (!task.title || !task.description || typeof task.completed !== 'boolean') {
+      return false;
     }
-    if (typeof task.completed !== 'boolean') {
-      throw new Error('Completed must be a boolean');
-    }
+    return true;
   };
 
 
 // GET /tasks
 app.get('/', (req, res) => {
-    res.json("welcome to tasks api");
+    res.status(200).send('Task Manager API');
   });
 
 // GET /tasks
 app.get('/tasks', (req, res) => {
-    res.status(200).json(tasksJson);
-  });
-  
-  
+    res.status(200).json(tasksArray);
+});
+
+
 // GET /tasks/:id
 app.get('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const task = tasksJson.tasks.find(t => t.id == id);
+    const id = parseInt(req.params.id);
+    const task = tasksJson.tasks.find(t => t.id === id);
+    console.log(task);
     if (!task) {
         res.status(404).json({ error: 'Task not found !!' });
     } else {
@@ -53,49 +55,44 @@ app.get('/tasks/:id', (req, res) => {
 
   // POST /tasks
 app.post('/tasks', (req, res) => {
-    try {
-            let task = req.body;
-            //validating the inputs
-            validateTask(task);
-            //generate new id value
-            task.id = generateId();
-            //push the task to task object
-            tasksJson.tasks.push(task);
-            //make the above changes to the file
-            fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
-            res.status(201).json(task);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
+    let task = req.body;
+    //validating the inputs
+    if (!validateTask(task)) {
+        return res.status(400).json('reqquired input fields missing');
     }
+    //generate new id value
+    task.id = generateId();
+    //push the task to task object
+    tasksJson.tasks.push(task);
+    //make the above changes to the file
+    fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
+    res.status(201).json(task);
   });
   
 
 // PUT /tasks/:id
 app.put('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const taskIndex = tasksJson.tasks.findIndex(t => t.id == id);
+    const id = parseInt(req.params.id);
+    const taskIndex = tasksJson.tasks.findIndex(t => t.id === id);
     if (taskIndex === -1) {
-        res.status(404).json({ error: 'Task not found !!' });
-    } else {
-        try {
-            //get the body from put
-            let updatedTaskDetails = req.body;
-            /*update the id field also otherwise, details will be updated w/o id field
-            shall optimize this to just replace with the data provided*/
-
-            //conver the string id to int
-            updatedTaskDetails.id = parseInt(id);
-            //validate the body 
-            validateTask(updatedTaskDetails);
-            //update the details in the taskJson object
-            tasksJson.tasks[taskIndex] = updatedTaskDetails;
-            //write the same changes to the file
-            fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
-            res.status(200).json(updatedTaskDetails);
-        } catch (err) {
-            res.status(400).json({ error: err.message });
-        }
+        return res.status(404).json({ error: 'Task not found !!' });
     }
+    //get the body from put
+    let updatedTaskDetails = req.body;
+    /*update the id field also otherwise, details will be updated w/o id field
+    shall optimize this to just replace with the data provided*/
+
+    //conver the string id to int
+    updatedTaskDetails.id = id;
+    //validate the body 
+    if (!validateTask(updatedTaskDetails)) {
+        return res.status(400).json('Required input fields missing or invalid');
+    }
+    //update the details in the taskJson object
+    tasksJson.tasks[taskIndex] = updatedTaskDetails;
+    //write the same changes to the file
+    fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
+    res.status(200).json(updatedTaskDetails);
 });
 
 
@@ -104,14 +101,13 @@ app.delete('/tasks/:id', (req, res) => {
     const id = req.params.id;
     const taskIndex = tasksJson.tasks.findIndex(t => t.id == id);
     if (taskIndex === -1) {
-        res.status(404).json({ error: 'Task not found' });
-    } else {
-        //remove the task using its position in the json array
-        tasksJson.tasks.splice(taskIndex, 1);
-        //write the same changes to the file
-        fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
-        res.status(200).json(`task with id ${id} deleted !!`)
-    }
+        return res.status(404).json({ error: 'Task not found' });
+    } 
+    //remove the task using its position in the json array
+    tasksJson.tasks.splice(taskIndex, 1);
+    //write the same changes to the file
+    fs.writeFileSync('./task.json', JSON.stringify(tasksJson));
+    res.status(200).json(`task with id ${id} deleted !!`)
   });
 
 app.listen(port, (err) => {
